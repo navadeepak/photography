33796d7c2f7042723d193b40208f1c92652fe8d3
navadeepak <navadeepakaswin007@gmail.com>
2024-11-06 22:51:22 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import HomePage from "./page/HomePage";
+import Contact from "./component/Contact";
+import Navbar from "./component/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <HomePage />
+        <Contact />
+      </div>
     </div>
   );
 }
